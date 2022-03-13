c43b854c5bdebbede700aa20919afc99f1d1d5dd
JhessReyes <jhessaureyes@gmail.com>
2022-03-12 18:25:22 -0600

    tabla unidades de medidas

---
+++ tienda/app/Http/Controllers/UnidadesDeMedidaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\unidades_de_medida;
+use Illuminate\Http\Request;
+
+class UnidadesDeMedidaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\unidades_de_medida  $unidades_de_medida
+     * @return \Illuminate\Http\Response
+     */
+    public function show(unidades_de_medida $unidades_de_medida)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\unidades_de_medida  $unidades_de_medida
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(unidades_de_medida $unidades_de_medida)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\unidades_de_medida  $unidades_de_medida
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, unidades_de_medida $unidades_de_medida)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\unidades_de_medida  $unidades_de_medida
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(unidades_de_medida $unidades_de_medida)
+    {
+        //
+    }
+}
---
+++ tienda/database/migrations/2022_03_10_025502_create_unidades_de_medidas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUnidadesDeMedidasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('unidades_de_medidas', function (Blueprint $table) {
+            $table->bigIncrements("codigo");
+            $table->string("descripcion");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('unidades_de_medidas');
+    }
+}
