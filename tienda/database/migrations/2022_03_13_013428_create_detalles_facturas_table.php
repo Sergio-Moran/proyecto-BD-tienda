a0538a1cacca72fdafb6047422e291ad53482a3b
JhessReyes <jhessaureyes@gmail.com>
2022-03-12 19:37:22 -0600

    tabla detalles facturas

---
+++ tienda/database/migrations/2022_03_13_013428_create_detalles_facturas_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetallesFacturasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detalles_facturas', function (Blueprint $table) {
+            $table->bigIncrements('codigo');
+            $table->foreignId('cod_producto')
+                ->nullable(true)
+                ->constrained('productos', 'codigo')
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
+            $table->foreignId('cod_factura')
+                ->nullable(true)
+                ->constrained('facturas', 'codigo')
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
+            $table->double('subtotal');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detalles_facturas');
+    }
+}
