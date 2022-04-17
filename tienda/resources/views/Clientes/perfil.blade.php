d2d5ccc9d3b85b4c89717386747c018caf35088e
Sergio Moran <sergioMoran@gmail.com>
2022-04-17 16:22:18 -0600

    Arreglos

--- tienda/resources/views/Clientes/perfil.blade.php
+++ tienda/resources/views/Clientes/perfil.blade.php
@@ -1,1 +1,19 @@
-<div>hola</div>
+<div class="form-row form-row shadow-md bg-auto rounded-lg " style="margin: 2ch">
+    <div class="avatar">
+        <div class="w-29 mask mask-hexagon">
+            <img src="https://api.lorem.space/image/face?hash=55350"/>
+        </div>
+    </div>
+
+    <div class="grid grid-cols-2 gap-4">
+        <label for="Nombre">Nombre Producto</label>
+        <input type="text" placeholder="Nombre Producto" name="nombres" class="input input-bordered">
+
+        <label for="Puesto">Puesto</label>
+        <input type="text" placeholder="Q" name="precio_venta" class="input input-bordered">
+
+        <label for="Algo mas">Algo mas</label>
+        <input type="text" placeholder="Q" name="precio_compra" class="input input-bordered">
+    </div>
+
+</div>
