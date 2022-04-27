6814f2e2e9509e6fbce166ecdb4f8078fb7581b3
Sergio Moran <sergioMoran@gmail.com>
2022-04-27 17:18:16 -0600

    Arreglos del perfil

--- tienda/app/Http/Livewire/Cliente/Index.php
+++ tienda/app/Http/Livewire/Cliente/Index.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Livewire\Cliente;
 
+use App\Models\dato_usuarios;
 use Livewire\Component;
 
 class Index extends Component
 {
+    public $valor = "";
+    public $porPagina = 100;
+
     public function render()
     {
-        return view('Clientes.perfil');
+        $usuarios = dato_usuarios::where('users.cod_dato_usuario_fk','=',request()->user()->id)
+        ->join('users', 'dato_usuarios.codigo', '=', 'users.cod_dato_usuario_fk')
+            ->limit($this->porPagina)
+            ->get();
+
+        return view('Clientes.perfil', ['usuarios' => $usuarios]);
     }
 }
--- tienda/resources/views/Clientes/perfil.blade.php
+++ tienda/resources/views/Clientes/perfil.blade.php
@@ -1,17 +1,35 @@
-<div class="form-row shadow-md bg-auto rounded-lg place-self-auto" style="margin: 2ch">
-    <div class="avatar grid justify-items-center">
-        <div class="w-29 rounded">
-            <img src="https://api.lorem.space/image/face?hash=92048" />
-        </div>
+<h1 class="btn btn-ghost normal-case text-xl" style="margin-top: 0.5ch; margin-left: 30%; font-size: 2rem">PERFIL</h1>
+<br>
 
+<div class="form-row form-row shadow-md bg-auto rounded-lg " style="margin: 2ch;  padding: 8ch">
+    <div class="grid grid-cols-2 gap-4">
+        <div class="avatar grid justify-items-center">
+            <div class="w-29 rounded">
+                <img src="https://api.lorem.space/image/face?hash=92048" />
+            </div>
+        </div>
         <div class="grid grid-cols-2 gap-4">
             <div class="grid justify-items-center">
-                <label for="Nombre">Nombre</label>
-                <input type="text" placeholder="Nombre" value="{{}}" class="input input-bordered">
-                <label for="Puesto">Puesto</label>
-                <input type="text" placeholder="Puesto" name="puesto" class="input input-bordered">
-                <label for="Algo mas">Algo mas</label>
-                <input type="text" placeholder="Algo mas" name="" class="input input-bordered">
+                @foreach ($usuarios as $usuario)
+                    <label for="Nombre" class="input-group input-group-lg">Nombre</label>
+                    <input type="text" placeholder="Nombre" value="{{ $usuario->nombres }}"
+                        class="input input-bordered" readonly>
+
+                    <label for="apellidos" class="input-group input-group-lg">Apellidos</label>
+                    <input type="text" value="{{ $usuario->apellidos }}" class="input input-bordered" readonly>
+
+                    <label for="Puesto" class="input-group input-group-lg">Puesto</label>
+                    <input type="text" value="{{ $usuario->rol }}" class="input input-bordered" readonly>
+
+                    <label for="Puesto" class="input-group input-group-lg">Correo</label>
+                    <input type="text" value="{{ $usuario->email }}" class="input input-bordered" readonly>
+
+                    <label for="dpi" class="input-group input-group-lg">DPI</label>
+                    <input type="text" value="{{ $usuario->dpi }}" class="input input-bordered" readonly>
+
+                    <label for="telefono" class="input-group input-group-lg">Teléfono</label>
+                    <input type="text" value="{{ $usuario->telefono }}" class="input input-bordered" readonly>
+                @endforeach
             </div>
         </div>
     </div>
