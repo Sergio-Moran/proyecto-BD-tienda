a8e22758bfedc0aa71584609b676873f8343c8ec
Sergio Moran <sergioMoran@gmail.com>
2022-05-04 16:34:26 -0600

    Arreglos

--- tienda/resources/views/Usuarios/formulario.blade.php
+++ tienda/resources/views/Usuarios/formulario.blade.php
@@ -12,28 +12,28 @@
     <div class="form-row form-row shadow-md bg-auto rounded-lg " style="margin: 2ch;  padding: 3ch">
         <div class="grid grid-cols-2 gap-4">
             <label for="Nombre">Nombre Usuario</label>
-            <input type="text" name="name" class="input input-bordered">
+            <input type="text" name="name" class="input input-bordered" required>
 
             <label for="Apellidos">Apellidos</label>
-            <input type="text" name="apellidos" class="input input-bordered">
+            <input type="text" name="apellidos" class="input input-bordered" required>
 
             <label for="Telefono">Telefono</label>
-            <input type="number" name="telefono" class="input input-bordered">
+            <input type="number" name="telefono" class="input input-bordered" required>
 
             <label for="Direccion">Dirección</label>
-            <input type="text" name="direccion" class="input input-bordered">
+            <input type="text" name="direccion" class="input input-bordered" required>
 
             <label for="dpi">DPI</label>
-            <input type="number" name="dpi" class="input input-bordered">
+            <input type="number" name="dpi" class="input input-bordered" required>
 
             <label for="Correo">Correo</label>
-            <input type="email" name="email" class="input input-bordered">
+            <input type="email" name="email" class="input input-bordered" required>
             
             <label for="Nombre">Contraseña</label>
-            <input type="password" name="password" class="input input-bordered">
+            <input type="password" name="password" class="input input-bordered" required>
 
             <label for="Nombre">Confirmar la Contraseña</label>
-            <input type="password" name="password" class="input input-bordered">
+            <input type="password" name="password" class="input input-bordered" required>
 
             <label for="rol">Rol</label>
             <select class="input input-bordered" name="rol" id="rol">
