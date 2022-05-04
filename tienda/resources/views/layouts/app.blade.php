eafec81fad2464e47409e0f66b87fc28597f86d5
Sergio Moran <sergioMoran@gmail.com>
2022-05-04 12:23:27 -0600

    reportes

--- tienda/resources/views/layouts/app.blade.php
+++ tienda/resources/views/layouts/app.blade.php
@@ -18,20 +18,15 @@
                     </a>
                 </li>
                 <li class="flex space-x-2 mt-2 cursor-pointer hover:text-[#EC5252] duration-150">
-                    <a class="btn btn-primary" name="btn-" id="btn-" type="button" href="{{ url('/Productos/Show') }}">
-                        <i class="">📦 Productos</i>
-                    </a>
-                </li>
-                <li class="flex space-x-2 mt-2 cursor-pointer hover:text-[#EC5252] duration-150">
-                    <a class="btn bg-info" style="padding-right: 44px" style="padd" name="btn-" id="btn-"
-                        type="button" href="{{ url('/Cliente/Show') }}">
-                        <i class="">🧑‍🔧 Cuenta</i>
-                    </a>
-                </li>
-                <li class="flex space-x-2 mt-2 cursor-pointer hover:text-[#EC5252] duration-150">
                     <a class="btn bg-info" style="padding-right: 44px" style="padd" name="btn-" id="btn-"
                         type="button" href="{{ url('/Proveedor') }}">
                         <i class="">🧑‍🔧 Proveedores</i>
+                    </a>
+                </li>
+                <li class="flex space-x-2 mt-2 cursor-pointer hover:text-[#EC5252] duration-150">
+                    <a class="btn btn-primary" name="btn-" id="btn-" type="button"
+                        href="{{ url('/Productos/Show') }}">
+                        <i class="">📦 Productos</i>
                     </a>
                 </li>
                 <li class="flex space-x-2 mt-2 cursor-pointer hover:text-[#EC5252] duration-150">
@@ -55,6 +50,12 @@
                     <a class="btn bg-success form-control" style="padding-right: 34px" name="btn-" id="btn-"
                         type="button" href="{{ url('/Cliente/Reportes') }}">
                         <i class="">📄 Reportes</i>
+                    </a>
+                </li>
+                <li class="flex space-x-2 mt-2 cursor-pointer hover:text-[#EC5252] duration-150">
+                    <a class="btn bg-info" style="padding-right: 44px" style="padd" name="btn-" id="btn-"
+                        type="button" href="{{ url('/Cliente/Show') }}">
+                        <i class="">🧑‍🔧 Cuenta</i>
                     </a>
                 </li>
                 @if (request()->user()->rol == 'Administrador')
