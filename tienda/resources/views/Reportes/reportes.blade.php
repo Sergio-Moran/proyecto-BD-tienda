1ef3d47df394e4cc60b2609b5d8cee824cecacf3
Sergio Moran <sergioMoran@gmail.com>
2022-05-04 12:15:25 -0600

    reportes

--- tienda/resources/views/Reportes/reportes.blade.php
+++ tienda/resources/views/Reportes/reportes.blade.php
@@ -1,22 +1,22 @@
 <div class="place-self-auto grid justify-items-center " style="margin: 2ch;  padding: 3ch">
     <div class="card w-96 bg-base-100 shadow-xl flex flex-col space-y-4 ">
         <div class="card-actions justify-center">
-            <a href="{{url('Facturas/index')}}" class="btn btn-primary">Reporte de Facturas</a>
+            <a href="{{url('Facturas/index')}}" target="blank" class="btn btn-primary">Reporte de Facturas</a>
         </div>
         <div class="card-actions justify-center">
-            <a href="{{url('/ReporteUno')}}" class="btn btn-primary">Reporte 1</a>
+            <a href="{{url('/ReporteUno')}}" target="blank" class="btn btn-primary">Reporte 1</a>
         </div>
         <div class="card-actions justify-center">
-            <a href="{{url('/ReporteDos')}}" class="btn btn-primary">Reporte 2</a>
+            <a href="{{url('/ReporteDos')}}" target="blank" class="btn btn-primary">Reporte 2</a>
         </div>
         <div class="card-actions justify-center">
-            <a href="{{url('/ReporteTres')}}" class="btn btn-primary">Reporte 3</a>
+            <a href="{{url('/ReporteTres')}}" target="blank" class="btn btn-primary">Reporte 3</a>
         </div>
         <div class="card-actions justify-center">
-            <a href="{{url('/ReporteCuatro')}}" class="btn btn-primary">Reporte 4</a>
+            <a href="{{url('/ReporteCuatro')}}" target="blank" class="btn btn-primary">Reporte 4</a>
         </div>
         <div class="card-actions justify-center">
-            <a href="{{url('/ReporteCinco')}}" class="btn btn-primary">Reporte 5</a>
+            <a href="{{url('/ReporteCinco')}}" target="blank" class="btn btn-primary">Reporte 5</a>
         </div>
     </div>
 </div>
